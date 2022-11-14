687dae041b4f3e2496e8f099ceaa2ca6a17ceb11
Sadid <sadid.jones@gmail.com>
2022-11-14 19:15:41 +0600

    @14/11/2022

--- complete-node-bootcamp-master/complete-node-bootcamp-master/4-natours/starter/app.js
+++ complete-node-bootcamp-master/complete-node-bootcamp-master/4-natours/starter/app.js
@@ -5,9 +5,14 @@ const app = express();
 const tourRouter = require('./routes/tourRoute');
 const userRouter = require('./routes/userRoutes');
 //1) Middleware
-app.use(morgan('dev'));
-
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
+
+app.use(express.static(`${__dirname}/public`));
+
 app.use((req, res, next) => {
   console.log(req);
   console.log('Hello from the Middleware 💥');
