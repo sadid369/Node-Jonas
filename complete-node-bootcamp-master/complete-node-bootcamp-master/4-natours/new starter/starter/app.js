9ddca9923b6cca61a5602421c762d8deb043b4a0
Sadid <sadid.jones@gmail.com>
2022-12-24 21:42:16 +0600

    @24/12/2022

--- complete-node-bootcamp-master/complete-node-bootcamp-master/4-natours/new starter/starter/app.js
+++ complete-node-bootcamp-master/complete-node-bootcamp-master/4-natours/new starter/starter/app.js
@@ -2,13 +2,6 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: `Hello from server side`, app: 'Natours' });
-});
-app.post('/', (req, res) => {
-  res.send(`Post in this URL`);
-});
-
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`App running on port at ${PORT}`);
