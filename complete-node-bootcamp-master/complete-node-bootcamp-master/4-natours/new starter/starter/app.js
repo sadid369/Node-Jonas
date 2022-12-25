5ba4e3f5fa526e375276b19a1d9e78dff93c335c
Sadid <sadid.jones@gmail.com>
2022-12-25 14:47:24 +0600

    @25/12/2022

--- complete-node-bootcamp-master/complete-node-bootcamp-master/4-natours/new starter/starter/app.js
+++ complete-node-bootcamp-master/complete-node-bootcamp-master/4-natours/new starter/starter/app.js
@@ -1,7 +1,36 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
-
+app.use(express.json());
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours', (req, res) => {
+  res.json({
+    message: 'Success',
+    result: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`App running on port at ${PORT}`);
