b4c5d51b9ef1a8ab677fd16e9e12752112efe043
Sadid <sadid.jones@gmail.com>
2023-01-16 20:41:59 +0600

    @16/01/2023

--- complete-node-bootcamp-master/complete-node-bootcamp-master/4-natours/new starter/starter/models/tourModel.js
+++ complete-node-bootcamp-master/complete-node-bootcamp-master/4-natours/new starter/starter/models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
+const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -77,6 +78,31 @@ const tourSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    startLocation: {
+      //GeoJson
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point'],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point'],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
+    guides: Array,
   },
   {
     toJSON: { virtuals: true },
@@ -89,6 +115,12 @@ tourSchema.virtual('durationWeeks').get(function () {
 //Document Middleware: runs before .save() and .create()
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 // tourSchema.pre('save', function (next) {
