4689260123fec2d8ae87ca2e8d8385d38724ec39
Sadid <sadid.jones@gmail.com>
2023-01-23 19:35:56 +0600

    @23/01/2023

--- complete-node-bootcamp-master/complete-node-bootcamp-master/4-natours/new starter/starter/controllers/handlerFactory.js
+++ complete-node-bootcamp-master/complete-node-bootcamp-master/4-natours/new starter/starter/controllers/handlerFactory.js
@@ -73,6 +73,7 @@ exports.getAll = (Model) =>
       .limitFields()
       .paginate();
 
+    // const doc = await features.query.explain();
     const doc = await features.query;
     //SEND QUERY
     res.status(200).json({
